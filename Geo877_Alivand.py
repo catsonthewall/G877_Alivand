ecf2a291c2b08bbf315f42b6e74ede7a4352cab7
fabio-at-uni <161589003+fabio-at-uni@users.noreply.github.com>
2024-05-22 16:19:06 +0200

    Update Geo877_Alivand.py

--- Geo877_Alivand.py
+++ Geo877_Alivand.py
@@ -2,8 +2,7 @@
 
 # Use this python file for all the "background" code of the project such as functions and classes.
 
-# This is the full graph-routing.py file 
-# which includes the different graph classes with their respective algorithms 
+# This is the full graph-routing.py file which includes the different graph classes with their respective algorithms 
 import geopandas as gpd
 import numpy as np
 
@@ -305,3 +304,42 @@ if __name__ == "__main__":
     matrix_graph = preprocess_gdf(gdf, graph=Graph_matrix())
     print(matrix_graph.dis_matrix)
     print(matrix_graph.nodes[0].pos)
+
+# Point class for scenicness scoring
+
+class Point():
+    # initialise
+    def __init__(self, x=None, y=None, cat=None):
+        self.x = x
+        self.y = y
+        self.cat = cat
+
+     # representation
+    def __repr__(self):
+        return f'Point(x={self.x}, y={self.y})'
+
+    # calculate Euclidean distance between two points
+    def distEuclidean(self, other):
+        return np.sqrt((self.x-other.x)**2 + (self.y-other.y)**2)
+    
+    # method to find the closest pixel center
+    def find_closest_pixel(self, pixel_centers):
+        distances = [self.distEuclidean(center) for center in pixel_centers]
+        closest_index = distances.index(min(distances))
+        closest_pixel_center = pixel_centers[closest_index]
+        return closest_pixel_center
+    
+    # method to find the neighbors of a given pixel center
+    def find_neighbors(self):
+        neighbors = []
+        for dx in [-100, 0, 100]:
+            for dy in [-100, 0, 100]:
+                neighbor_point = Point(self.x + dx, self.y + dy, self.cat)
+                neighbors.append(neighbor_point)
+        return neighbors
+    
+    # static method to get landcover values for neighborhood
+    @staticmethod
+    def get_landcover(neighborhood):
+        values = [point.cat for point in neighborhood]
+        return values
